1978918605d5d65241516dfefa99a3cac675c6af
holladortun <holladortun.omotoso@gmail.com>
2023-06-07 15:23:57 +0100

    updated sign up page

--- Client/src/pages/sign-up/Sign-Up.jsx
+++ Client/src/pages/sign-up/Sign-Up.jsx
@@ -1,58 +1,74 @@
-import { Link } from "react-router-dom"
-import "./Sign-Up.css"
-import { useState } from "react"
-import axios from "axios"
+import { Link } from "react-router-dom";
+import "./Sign-Up.css";
+import { useState } from "react";
+import axios from "axios";
 
 export default function SignUp() {
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState("")
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(false);
     try {
-      const res = await axios.post("/auth/register", {
-        username,
-        email,
-        password,
-      });
-      res.data && window.location.replace("/login");
+      const res = await axios.post(
+        "https://olayiimiikas-blog-app-api.onrender.com/api/auth/register",
+        {
+          username,
+          email,
+          password,
+        }
+      );
+      res.data &&
+        window.location.replace(
+          "https://olayiimiikas-blog-app-api.onrender.com/api/login"
+        );
     } catch (err) {
-      setError(true) 
+      setError(true);
     }
   };
   return (
-    <div className='Sign-Up'>
-            <span className="Sign-UpTitle">Register</span>
-            <form className="Sign-UpForm" onSubmit={handleSubmit}>            <label>Username</label>
-            <input 
-            type="text" 
-            className="Sign-UpInput" 
-            placeholder="Enter your username..." 
-            onChange={e=>setUsername(e.target.value)}
-            />
-            <label>Email</label>
-            <input
-             type="email" 
-             className="Sign-UpInput" 
-             placeholder="Enter your email..." 
-             onChange={e=>setEmail(e.target.value)}
-             />
-            <label>Password</label>
-            <input 
-            type="password" 
-            className="Sign-UpInput" 
-            placeholder="Enter your password..." 
-            onChange={e=>setPassword(e.target.value)}
-            />
-            <button className="Sign-UpButton" type="submit">Sign-Up</button>
-        </form>
-        <button className="Sign-UpSign-upButton">
-          <Link className="link" to="/Login">LOGIN</Link>
+    <div className="Sign-Up">
+      <span className="Sign-UpTitle">Register</span>
+      <form className="Sign-UpForm" onSubmit={handleSubmit}>
+        {" "}
+        <label>Username</label>
+        <input
+          type="text"
+          className="Sign-UpInput"
+          placeholder="Enter your username..."
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          className="Sign-UpInput"
+          placeholder="Enter your email..."
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          className="Sign-UpInput"
+          placeholder="Enter your password..."
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="Sign-UpButton" type="submit">
+          Sign-Up
         </button>
-        {error && <span style={{color:"red", marginTop:"10px"}}>Something went wrong!</span>}
+      </form>
+      <button className="Sign-UpSign-upButton">
+        <Link className="link" to="/Login">
+          LOGIN
+        </Link>
+      </button>
+      {error && (
+        <span style={{ color: "red", marginTop: "10px" }}>
+          Something went wrong!
+        </span>
+      )}
     </div>
-  )
+  );
 }
